b2ed371bded8be123cc7988e17c4338c75b27f2c
fisseha-estifanos <fisseha.137@gmail.com>
2024-08-13 16:43:11 +0300

    main server added

---
+++ main.py
@@ -0,0 +1,48 @@
+import os
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from data_models import transfer_data_models
+import RagProcessor
+from utils import get_variables
+
+
+app = FastAPI(swagger_ui_parameters={"syntaxHighlight": True})
+
+
+@app.get('/')
+async def root():
+    return {
+        "status": "online",
+        "version": 0.01
+    }
+
+
+# region Rag
+@app.post('/fetch_answer_using_collections')
+async def fetch_answer_using_collections(
+            rag_data: transfer_data_models.FetchAnswerUsingCollections):
+    answer_fetcher = RagProcessor.RagProcessor()
+    status = await answer_fetcher.fetch_answer_from_rag_using_collections(
+        rag_data.question, rag_data.prompt, rag_data.use_model,
+        rag_data.company_name, rag_data.embedding_model_name,
+        rag_data.collection_name, rag_data.use_local_qdrant_vectorstore,
+        rag_data.vector_store_name)
+    print(f'\nstatus: {status}')
+    if status['status'] == 'failed':
+        raise HTTPException(status_code=status['status_code'],
+                            detail=status['error_message'])
+    elif status['status'] == 'success':
+        return {
+            "answer": status['answer'],
+            "context": status['context']
+        }
+# endregion Rag
+
+if __name__ == '__main__':
+    get_variables()
+    print(f'main server::- starting: {app}')
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app="main:app",
+                host="0.0.0.0",
+                port=port)
+    print(f'main server::- over and out: {app}')
